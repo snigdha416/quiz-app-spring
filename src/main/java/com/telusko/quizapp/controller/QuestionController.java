d075e3d458b35ef52e2d0c469af6b7a89bed3cff
Snigdha Kantlam <kantlamsnigdha@gmail.com>
2025-05-20 11:57:03 +0530

    messages from VS code

--- src/main/java/com/telusko/quizapp/controller/QuestionController.java
+++ src/main/java/com/telusko/quizapp/controller/QuestionController.java
@@ -10,6 +10,9 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+
+// controller for quiz
+
 @RequestMapping("question")
 public class QuestionController {
 
